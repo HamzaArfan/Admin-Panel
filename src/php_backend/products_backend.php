ef3cf1f553aa167a8fb4bfb1b770ebd77344be7f
Hamza Arfan <hamzaarfanfazal2350@gmail.com>
2024-08-13 13:59:11 +0500

    new changes

--- src/php_backend/products_backend.php
+++ src/php_backend/products_backend.php
@@ -1,6 +1,6 @@
 <?php
         include "dbcon.php";
         $result = mysqli_query($conn, "SELECT * FROM `products`");
-        $result2 = $conn->query("SELECT * FROM products WHERE product_id IN (SELECT r1.product_id FROM reviews AS r1 JOIN reviews AS r2 ON r1.product_id = r2.product_id GROUP BY r1.product_id HAVING AVG(r2.rating) > 4)");
-        $result3 = $conn->query("SELECT p1.category, sum(o2.total) as totalsum from  products p1 join orderitems o1 on p1.product_id = o1.product_id join  orders o2 on o1.order_id = o2.order_id group by  p1.category" );
+        $result2 = mysqli_query($conn,"SELECT * FROM products WHERE product_id IN (SELECT r1.product_id FROM reviews AS r1 JOIN reviews AS r2 ON r1.product_id = r2.product_id GROUP BY r1.product_id HAVING AVG(r2.rating) > 4)");
+        $result3 = mysqli_query($conn,"SELECT p1.category, sum(o2.total) as totalsum from  products p1 join orderitems o1 on p1.product_id = o1.product_id join  orders o2 on o1.order_id = o2.order_id group by  p1.category" );
 ?>
--- src/php_backend/users_backend.php
+++ src/php_backend/users_backend.php
@@ -6,10 +6,10 @@
             $conn,
             "SELECT user_id ,is_deleted ,name,password, address FROM users WHERE user_id NOT IN ( SELECT user_id FROM orders )"
         );
-        $result2 = $conn->query(
+        $result2 = mysqli_query($conn,
             "SELECT u1.user_id, u1.name,is_deleted, MAX(o1.created_at) as datecreated FROM users u1 JOIN orders o1 ON u1.user_id = o1.user_id GROUP BY u1.user_id, u1.name"
         );
-        $result3 = $conn->query(
+        $result3 =  mysqli_query($conn,
             "SELECT o1.user_id, u1.name , u1.is_deleted from orders o1 join orderitems o2 on o1.order_id = o2.order_id join users u1 on o1.user_id = u1.user_id group by o1.user_id, u1.name having count(distinct o2.product_id) > 1"
         );
 ?>
